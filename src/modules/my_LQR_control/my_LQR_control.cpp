36caa9050a56864014e5cef820282461876e502d
JMshare <jm1e16@soton.ac.uk>
2019-09-14 21:49:04 +0100

    omg/10 if filter loss

--- src/modules/my_LQR_control/my_LQR_control.cpp
+++ src/modules/my_LQR_control/my_LQR_control.cpp
@@ -505,13 +505,13 @@ int My_LQR_control::filter_rates(){
         omg_filtered = lp_filter_angular_rates.apply(omg);
         filter_status = 0; // ok
         if(!(omg_filtered(0) > -1000000.0f && omg_filtered(1) > -1000000.0f && omg_filtered(2) > -1000000.0f)){ // safety check, if NAN this should come to false
-            omg_filtered = omg;
+            omg_filtered = omg/10.0f; // tone it down to prevent feeding vibrations to servos
             filter_status = 1; // whops
         }
     }
     else{
-        omg_filtered = omg;
-        filter_status = 2; // whops 2
+        omg_filtered = omg; 
+        filter_status = 2; // no cutoff
     }
 
     return PX4_OK;
