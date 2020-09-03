7f12a2aca6080fcbd68400b2464e2fffe018815b
JMshare <jm1e16@soton.ac.uk>
2020-09-03 10:31:53 +0100

    preset my_rpm_topic.status to nnz
    cos it's possible it inits as 0 and then the printouts it thinks the sensor is running

--- src/modules/my_LQR_control/my_LQR_control.cpp
+++ src/modules/my_LQR_control/my_LQR_control.cpp
@@ -1453,6 +1453,8 @@ k_scheds(9,0) =   1.40f; k_scheds(9,1) =   1.40f; k_scheds(9,2) =   2.30f; k_sch
     angular_rates_filtered.yaw = 0.0f;
     angular_rates_filtered.cutoff_freqn_eps = cutoff_freqn_eps;
 
+    my_rpm_topic.status = 1; // manually preset different to zero to make sure it's faulty if no updates
+
     Del_c_lim.setAll(1.0f);
 
     E2B.setAll(0.0f);
